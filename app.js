4fa67cd598ea76750ac3c7bc5830669f1873f2c9
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-03-14 14:37:10 -0600

    fix

--- app.js
+++ app.js
@@ -2,25 +2,27 @@ import express from "express";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import router from "./routes/index.js";
 
 dotenv.config();
-const db = process.env.DB;
+// const db = process.env.DB;
 
 const port = 3001;
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
+app.use(cors());
 
 app.use("/", router);
 
 mongoose
-    .connect(db)
+    .connect(process.env.MONGODB_URI)
     .then(() => {
         console.log("DB ok");
 
-        app.listen(port, (error) => {
+        app.listen(process.env.PORT || 3001, (error) => {
             if (error) {
                 return console.log(error);
             }
