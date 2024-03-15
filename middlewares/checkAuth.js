3b32d7260f04063db974eaa1cf9d5f0f8b5c8ada
Alex-HP <aleksandr.lozinskiy2005@gmail.com>
2024-03-15 10:57:47 -0600

    cookie getting settings

--- middlewares/checkAuth.js
+++ middlewares/checkAuth.js
@@ -3,7 +3,9 @@ import { UserModel } from "../models/User.js";
 
 export const checkAuth = async (req, res, next) => {
     try {
-        const tokenData = verify(req.cookies.token);
+        const tokenData = verify(
+            res.headers.get("Set-Cookie").split(";")[0].split("=")[1]
+        );
 
         const user = await UserModel.findById({ _id: tokenData.id });
 
